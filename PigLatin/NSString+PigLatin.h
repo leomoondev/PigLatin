00a8660d1565a5e956d4ab3100f17fd39a651aea
Leo Moon <leomoondev@gmail.com>
2017-03-05 18:34:46 -0800

    Completed the assignment

---
+++ PigLatin/NSString+PigLatin.h
@@ -0,0 +1,13 @@
+//
+//  NSString+PigLatin.h
+//  PigLatin
+//
+//  Created by Hyung Jip Moon on 2017-03-05.
+//  Copyright © 2017 leomoon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (PigLatin)
+
+@end
---
+++ PigLatin/NSString+PigLatin.m
@@ -0,0 +1,13 @@
+//
+//  NSString+PigLatin.m
+//  PigLatin
+//
+//  Created by Hyung Jip Moon on 2017-03-05.
+//  Copyright © 2017 leomoon. All rights reserved.
+//
+
+#import "NSString+PigLatin.h"
+
+@implementation NSString (PigLatin)
+
+@end
